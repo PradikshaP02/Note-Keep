27951327ebf472edec72122d91ed9b307d7809ad
PradikshaP02 <85931159+PradikshaP02@users.noreply.github.com>
2023-03-13 19:52:01 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-//import { getAnalytics } from "firebase/analytics";
 import "firebase/database";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
@@ -19,10 +16,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-//const db = firebaseConfig.database();
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 export { db };
-//const analytics = getAnalytics(app);
 
